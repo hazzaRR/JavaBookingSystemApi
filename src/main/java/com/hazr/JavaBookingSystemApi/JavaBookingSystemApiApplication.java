7f972f834b1b31977914e4e5277a044444630ab9
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-07-16 21:26:53 +0100

    added boilerplate code for springboot app and basic file structure layout

---
+++ src/main/java/com/hazr/JavaBookingSystemApi/JavaBookingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.hazr.JavaBookingSystemApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBookingSystemApiApplication.class, args);
+	}
+
+}
