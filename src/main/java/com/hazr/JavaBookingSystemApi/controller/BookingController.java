0dc00eb95108e5b6f866371a0a26853bbd4fa27f
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-07-22 18:32:24 +0100

    added necessary controller classes to interact with the database

---
+++ src/main/java/com/hazr/JavaBookingSystemApi/controller/BookingController.java
@@ -0,0 +1,9 @@
+package com.hazr.JavaBookingSystemApi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/booking")
+public class BookingController {
+}
